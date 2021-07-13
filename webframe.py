ceb79ae16f09eb66bd3772ca52d210de6050696a
Toluwa Olaleye <tao44@cornell.edu>
2021-07-13 19:13:26 +0000

    User Input

--- webframe.py
+++ webframe.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '87cf84f22b2e953029103bea6c3e2f57'
 
 @app.route("/")
 def hello_world():
@@ -8,8 +10,15 @@ def hello_world():
 @app.route("/second_page")
 def second_page():
     return render_template('second_page.html', subtitle='Second Page', text='This is the second page')
-    
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
     
-"WOWW"
